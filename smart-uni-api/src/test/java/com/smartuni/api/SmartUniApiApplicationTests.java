ca72916fa3e9428eeec91c46da3a230c8f8f5c46
Heshal <heshal.dissanayake1@gmail.com>
2026-03-28 05:41:46 +0530

    initial: project scaffold - Spring Boot API + React client, folder structure, base config

---
+++ smart-uni-api/src/test/java/com/smartuni/api/SmartUniApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smartuni.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartUniApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
