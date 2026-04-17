872a1f4882e700483bf704e6b9ae1639ac6966d6
CharyaSamadhi <charyasamadhi234@gmail.com>
2026-04-18 01:47:36 +0530

    fix routing setup and remove duplicate BrowserRouter

--- smart-uni-client/src/App.jsx
+++ smart-uni-client/src/App.jsx
@@ -39,36 +39,6 @@ function ProtectedAppLayout() {
       <AppLayout />
     </NotificationProvider>
   );
-import { Link, Navigate, Route, Routes } from 'react-router-dom'
-import ResourceDetail from './pages/resources/ResourceDetail'
-import ResourceForm from './pages/resources/ResourceForm'
-import ResourceList from './pages/resources/ResourceList'
-import './App.css'
-
-function App() {
-  return (
-    <div className="app-shell">
-      <header className="top-nav">
-        <Link to="/resources" className="brand">
-          SmartUni Facilities
-        </Link>
-        <nav>
-          <Link to="/resources">Resources</Link>
-          <Link to="/resources/new">Add Resource</Link>
-        </nav>
-      </header>
-
-      <main>
-        <Routes>
-          <Route path="/" element={<Navigate to="/resources" replace />} />
-          <Route path="/resources" element={<ResourceList />} />
-          <Route path="/resources/new" element={<ResourceForm />} />
-          <Route path="/resources/:id" element={<ResourceDetail />} />
-          <Route path="/resources/:id/edit" element={<ResourceForm />} />
-        </Routes>
-      </main>
-    </div>
-  )
 }
 
 export default function App() {
